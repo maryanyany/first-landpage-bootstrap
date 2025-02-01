c832c4f55976b8004a70f89cde525a8739e44d52
maryanyany <maryanebezerraoliveira@gmail.com>
2025-02-01 16:42:54 -0300

    Feat: Interface adjustments and testing with JavaScrit and Node

--- javacript.js
+++ javacript.js
@@ -1,4 +1,4 @@
-// variavels
+a// variavels
 
 var a
 let b
@@ -7,6 +7,23 @@ const c = 20
 // não podemos reassinar uma varievel constante
 //c = 11
 
-// comunicação via terminal
+a = 'banana'
+b = 500
 
+// comunicação via terminal
+console.log(typeof a)
+console.log(a, b, c)
 console.log('rodei o script!')
+
+// tipode de dados
+
+let name = 'Lola'
+let age =120
+let open = false
+const person = {
+    name: 'Felipe',
+    age: 15,
+    angender: 'male'
+}
+
+//const persons = ['Felipe, Maria, João']
